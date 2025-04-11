10d2c1d85b235e99fb45298fc592b6a033c950f9
GustavoScot <gustavom.scot@gmail.com>
2025-04-11 00:29:56 -0300

    commit 00h

--- src/cidades/cidades.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CidadesService } from './cidades.service';
-import { CidadesController } from './cidades.controller';
-
-@Module({
-  controllers: [CidadesController],
-  providers: [CidadesService],
-})
-export class CidadesModule {}
--- src/estudantes/estudantes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EstudantesService } from './estudantes.service';
-import { EstudantesController } from './estudantes.controller';
-
-@Module({
-  controllers: [EstudantesController],
-  providers: [EstudantesService],
-})
-export class EstudantesModule {}
